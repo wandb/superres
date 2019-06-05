260693612255aafc361c9fff75a893ba61a35cdc
Chris Van Pelt <vanpelt@gmail.com>
2019-06-05 12:05:08 -0700

    Fixes for image logging

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras import layers
 from tensorflow.keras import backend as K
-from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.callbacks import Callback
 import wandb
 from wandb.keras import WandbCallback
 
@@ -50,15 +50,17 @@ def image_generator(batch_size, img_dir):
             counter = 0
         for i in range(batch_size):
             img = input_filenames[counter + i]
-            small_images[i] = img_to_array(Image.open(img))
-            large_images[i] = img_to_array(
-                Image.open(img.replace("-in.jpg", "-out.jpg")))
+            small_images[i] = np.array(Image.open(img)) / 255.0
+            large_images[i] = np.array(
+                Image.open(img.replace("-in.jpg", "-out.jpg"))) / 255.0
         yield (small_images, large_images)
         counter += batch_size
 
 
 def perceptual_distance(y_true, y_pred):
     """Calculate perceptual distance, DO NOT ALTER"""
+    y_true *= 255
+    y_pred *= 255
     rmean = (y_true[:, :, :, 0] + y_pred[:, :, :, 0]) / 2
     r = y_true[:, :, :, 0] - y_pred[:, :, :, 0]
     g = y_true[:, :, :, 1] - y_pred[:, :, :, 1]
@@ -66,6 +68,19 @@ def perceptual_distance(y_true, y_pred):
 
     return K.mean(K.sqrt((((512+rmean)*r*r)/256) + 4*g*g + (((767-rmean)*b*b)/256)))
 
+val_generator = image_generator(config.batch_size, val_dir)
+in_sample_images, out_sample_images = next(val_generator)
+
+class ImageLogger(Callback):
+    def on_epoch_end(self, epoch, logs):
+        preds = self.model.predict(in_sample_images)
+        in_resized = []
+        for arr in in_sample_images:
+            #Simple upsampling
+            in_resized.append(arr.repeat(8, axis=0).repeat(8, axis=1))
+        wandb.log({
+            "examples": [wandb.Image(np.concatenate([in_resized[i] * 255, o * 255, out_sample_images[i] * 255], axis=1)) for i, o in enumerate(preds)]
+        }, commit=False)
 
 model = Sequential()
 model.add(layers.Conv2D(3, (3, 3), activation='relu', padding='same',
@@ -81,10 +96,9 @@ model.add(layers.Conv2D(3, (3, 3), activation='relu', padding='same'))
 model.compile(optimizer='adam', loss='mse',
               metrics=[perceptual_distance])
 
-val_generator = image_generator(config.batch_size, val_dir)
 model.fit_generator(image_generator(config.batch_size, train_dir),
                     steps_per_epoch=config.steps_per_epoch,
                     epochs=config.num_epochs, callbacks=[
-                        WandbCallback(data_type='image', generator=val_generator, predictions=16)],
+                        ImageLogger(), WandbCallback()],
                     validation_steps=config.val_steps_per_epoch,
                     validation_data=val_generator)
